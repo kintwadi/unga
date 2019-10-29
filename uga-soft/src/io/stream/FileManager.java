55cc665d261c13803b47f3a3b3683f15b41b4884
MabialaA <antmabiala@gmail.com>
2019-10-29 16:43:51 +0100

    ini

---
+++ uga-soft/src/io/stream/FileManager.java
@@ -0,0 +1,23 @@
+//https://docs.oracle.com/javase/tutorial/essential/io/bytestreams.html
+package io.stream;
+
+
+
+public class FileManager
+{
+
+    public void fileCopy(String src, String dest) {
+        
+        
+    }
+    public static void main( String[] args )
+    {
+        // TODO Auto-generated method stub
+
+    }
+  
+
+}
+
+// -------------------------------------------------------------------------
+// end of class FileManager.java
