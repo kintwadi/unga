03d522ac8af24b7fd1221021e4d775c5bcbb5115
MabialaA <antmabiala@gmail.com>
2019-10-30 12:21:32 +0100

    copyFile example

--- uga-soft/src/io/stream/FileManager.java
+++ uga-soft/src/io/stream/FileManager.java
@@ -1,22 +1,53 @@
-//https://docs.oracle.com/javase/tutorial/essential/io/
-//https://docs.oracle.com/javase/tutorial/essential/io/bytestreams.html
+
 package io.stream;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
+public class FileManager {
 
-public class FileManager
-{
+	public void fileCopy(String sourceFile, String destinationFile) throws Exception {
 
-    public void fileCopy(String src, String dest) {
-        
-        
-    }
-    public static void main( String[] args )
-    {
-        // TODO Auto-generated method stub
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
 
-    }
-  
+		try {
+
+			fis = new FileInputStream(sourceFile);
+			fos = new FileOutputStream(destinationFile);
+			int data = 0;
+
+			// read() ==> returns the next byte or -1 if end of file
+
+			while ((data = fis.read()) != -1) {
+
+				fos.write(data);
+				
+			}
+
+		} finally {
+
+			if (fis != null) {
+				fis.close();
+
+			}
+			if (fos != null) {
+
+				fos.close();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		String sourceFile = "./sourceFile";
+		String destinationFile = "./destinationFile";
+	
+		FileManager fileManager = new FileManager();
+		fileManager.fileCopy(sourceFile, destinationFile);
+
+	}
 
 }
 
